ec9077a7611842368a008032b08dbd9812a787cd
AkashBhave <akashbbhave@gmail.com>
2018-08-13 15:05:26 -0400

    Read ratings file into DataFrame

--- engine.py
+++ engine.py
@@ -2,18 +2,33 @@ import urllib.request
 import zipfile
 import os
 
+import pandas as pd
+import numpy as np
+
+datasetName = ""
+
+
 def getDataset(datasetType):
     smallDatasetName = "ml-latest-small"
     bigDatasetName = "ml-latest"
     smallDatasetNewName = "data-small"
     bigDatasetNewName = "data-big"
-    smallDatasetURL = "http://files.grouplens.org/datasets/movielens/" + smallDatasetName + ".zip"
-    bigDatasetURL = "http://files.grouplens.org/datasets/movielens/" + bigDatasetName + ".zip"
+    smallDatasetURL = "http://files.grouplens.org/datasets/movielens/" + \
+        smallDatasetName + ".zip"
+    bigDatasetURL = "http://files.grouplens.org/datasets/movielens/" + \
+        bigDatasetName + ".zip"
+
+    global datasetName
 
     if datasetType == "small":
-        if not checkDataset(smallDatasetNewName): downloadDataset(smallDatasetURL, smallDatasetName, smallDatasetNewName)
+        datasetName = smallDatasetNewName
+        if not checkDataset(smallDatasetNewName):
+            downloadDataset(smallDatasetURL, smallDatasetName, smallDatasetNewName)
     else:
-        if not checkDataset(bigDatasetNewName): downloadDataset(bigDatasetURL, bigDatasetName, bigDatasetNewName)
+        datasetName = bigDatasetNewName
+        if not checkDataset(bigDatasetNewName):
+            downloadDataset(bigDatasetURL, bigDatasetName, bigDatasetNewName)
+            
 
 
 def checkDataset(datasetNewName):
@@ -21,6 +36,7 @@ def checkDataset(datasetNewName):
         return True
     else:
         return False
+
 
 def downloadDataset(datasetURL, datasetName, datasetNewName):
     urllib.request.urlretrieve(datasetURL, datasetNewName + ".zip")
@@ -30,8 +46,16 @@ def downloadDataset(datasetURL, datasetName, datasetNewName):
     os.rename(datasetName, datasetNewName)
     os.remove(datasetNewName + ".zip")
 
+
+def readCSV():
+    ratingsFrameCols = ["user_id", "movie_id", "rating"]
+    ratingsFrame = pd.read_csv(datasetName + "/ratings.csv", sep=",", header=0, names=ratingsFrameCols, usecols=range(3))
+    ratingsFrame = ratingsFrame.groupby("movie_id").agg({"rating": [np.size, np.mean]})
+
+
 def main():
     getDataset("small")
+    readCSV()
 
 if __name__ == '__main__':
     main()
